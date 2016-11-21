dd00c55805833470415a90be57249d5dc2ea0b66
Gabin <hello@gabinaureche.com>
2016-11-21 09:18:25 +0100

    convert to es5 for broader browser support

--- app.js
+++ app.js
@@ -1,78 +1,82 @@
 /* global fetch, FileReader */
 
-let accessToken;
-let postID;
-const reactions = ['LIKE', 'LOVE', 'WOW', 'HAHA', 'SAD', 'ANGRY'];
+var accessToken;
+var postID;
+var reactions = ['LIKE', 'LOVE', 'WOW', 'HAHA', 'SAD', 'ANGRY'];
 
-const maxFails = 20;
-let fails = 0;
+var maxFails = 20;
+var fails = 0;
 function updateCounters() {
-  const query = reactions.map((reaction) => {
-    const code = 'reactions_' + reaction.toLowerCase();
-    return `reactions.type(${reaction}).limit(0).summary(total_count).as(${code})`;
+  var query = reactions.map(function(reaction) {
+    var code = 'reactions_' + reaction.toLowerCase();
+    return 'reactions.type(' + reaction + ').limit(0).summary(total_count).as(' + code + ')';
   }).join(',');
-  const endpoint = `https://graph.facebook.com/v2.8/?ids=${postID}&fields=${query}&access_token=${accessToken}`;
+  var endpoint = 'https://graph.facebook.com/v2.8/?ids=' + postID + '&fields=' + query + '&access_token=' + accessToken;
 
   fetch(endpoint)
-    .then((res) => res.json())
-    .then((res) => {
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(res) {
       if (res.error) {
         throw new Error(res.error.message);
       }
 
-      const post = res[postID];
+      var post = res[postID];
       reactions
-        .forEach((reaction) => {
-          const reactionCounter = document.querySelector(`[data-reaction-counter-${reaction.toLowerCase()}]`);
-          reactionCounter.textContent = post[`reactions_${reaction.toLowerCase()}`].summary.total_count;
+        .forEach(function(reaction) {
+          var reactionCounter = document.querySelector('[data-reaction-counter-' + reaction.toLowerCase() + ']');
+          reactionCounter.textContent = post['reactions_' + reaction.toLowerCase()].summary.total_count;
         })
       ;
     })
-    .catch((err) => console.error('An error occurred while fetching data from Facebook', err.message))
-    .then(() => {
+    .catch(function(err) {
+      console.error('An error occurred while fetching data from Facebook', err.message);
+    })
+    .then(function() {
       fails += 1;
 
       if (fails < maxFails) {
         setTimeout(updateCounters, 5000);
       } else {
-        console.error(`Failed to fetch data from Facebook ${maxFails} times, now aborting`);
+        console.error('Failed to fetch data from Facebook ' + maxFails + ' times, now aborting');
       }
     })
   ;
 }
 
-const fileInputs = document.querySelectorAll('[type="file"]');
-fileInputs.forEach((input) => {
-  const wrapper = input.parentElement.parentElement;
+var fileInputs = document.querySelectorAll('[type="file"]');
+fileInputs.forEach(function(input) {
+  var wrapper = input.parentElement.parentElement;
 
-  input.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
+  input.addEventListener('change', function(event) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
 
-    reader.onload = (loadEvent) => {
-      const base64 = loadEvent.target.result;
-      wrapper.style.backgroundImage = `url('${base64}')`;
+    reader.onload = function(loadEvent) {
+      var base64 = loadEvent.target.result;
+      wrapper.style.backgroundImage = 'url(\'' + base64 + '\')';
     };
 
     reader.readAsDataURL(file);
   });
 });
 
-const modal = document.getElementById('modal');
-const form = document.querySelector('form');
-form.addEventListener('click', (event) => {
+var modal = document.getElementById('modal');
+var form = document.querySelector('form');
+form.addEventListener('click', function(event) {
   if (event.target.classList.contains('link')) {
     event.target.parentElement.style.display = 'none';
     event.target.parentElement.nextElementSibling.style.display = 'block';
   }
 });
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', function(event) {
   event.preventDefault();
-  const values = getFormValues(form, ['accessToken', 'appId', 'appSecret', 'postId']);
+  var values = getFormValues(form, ['accessToken', 'appId', 'appSecret', 'postId']);
 
   if (values.appId && values.appSecret) {
-    accessToken = `${values.appId}|${values.appSecret}`;
+    accessToken = values.appId + '|' + values.appSecret;
   } else if (values.accessToken) {
     accessToken = values.accessToken;
   }
@@ -88,8 +92,8 @@ form.addEventListener('submit', (event) => {
 });
 
 function getFormValues(form, names) {
-  return names.reduce((acc, name) => {
-    acc[name] = form.querySelector(`[name=${name}]`).value;
+  return names.reduce(function(acc, name) {
+    acc[name] = form.querySelector('[name=' + name + ']').value;
     return acc;
   }, {});
 }
